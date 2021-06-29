e5766209ef8267a5bdbef3671ab04be527a4fd85
orseL <orseLv99@gmail.com>
2021-06-29 15:46:57 +0900

    형태작업중

--- src/index.js
+++ src/index.js
@@ -1,76 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import moment from 'moment';
+import styles from './index.module.css';  // css naming
 
-class Square extends React.Component {
+//
+function Top(props) {
+  return (
+    <div className={styles.top}>
+      <h1>행동수집 정책편집 페이지</h1>
+      <button className={styles.topButton} onClick={props.onClick}>{props.value}</button>
+    </div>
+  );
+}
+function Mid(props) {
+  return (
+    <div className={styles.mid}>
+      <h2>ListView</h2>
+      <h2>ListView</h2>
+      <h2>ListView</h2>
+      <h2>ListView</h2>
+    </div>
+  );
+}
+
+function Bot(props) {
+  return (
+    <div className={styles.bot}>
+      <h2>Status : {props.value}</h2>
+    </div>
+  );
+}
+
+class Rule extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      result: "null",
     }
   }
-  
-  render() {
-    return (
-      <button className="square" onClick={ () => {
-        //alert(`CLICKED VALUE: ${this.props.value}`);
-        console.log('CLICKED');
-        this.setState({ value: 'X' });
-      }}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  storeRule(index) {
+    if (window.confirm(`test ${index}`) === true) {
+      // TODO : 실제 저장
+      this.setState({ result: `Success ${moment().format('MMMM Do YYYY, HH:mm:ss a')}` });
+    }
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+        <Top value="Save" onClick={() => this.storeRule(1)} />
+        <Mid />
+        <Bot value={this.state.result} />
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <Game />,
+  <Rule />,
   document.getElementById('root')
 );
